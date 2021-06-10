647934cd0d4850427fb50f934306bf93612d5a2d
= <=>
2021-06-10 16:12:22 +0700

    create auth controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+use Validator;
+use Hash;
+use Session;
+use App\Models\User;
+
+
+class AuthController extends Controller
+{
+    public function showFormLogin()
+    {
+        if (Auth::check()) { // true sekalian session field di users nanti bisa dipanggil via Auth
+            //Login Success
+            return redirect()->route('home');
+        }
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        $rules = [
+            'email'                 => 'required|email',
+            'password'              => 'required|string'
+        ];
+
+        $messages = [
+            'email.required'        => 'Email wajib diisi',
+            'email.email'           => 'Email tidak valid',
+            'password.required'     => 'Password wajib diisi',
+            'password.string'       => 'Password harus berupa string'
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if($validator->fails()){
+            return redirect()->back()->withErrors($validator)->withInput($request->all);
+        }
+
+        $data = [
+            'email'     => $request->input('email'),
+            'password'  => $request->input('password'),
+        ];
+
+        Auth::attempt($data);
+
+        if (Auth::check()) { // true sekalian session field di users nanti bisa dipanggil via Auth
+            //Login Success
+            return redirect()->route('home');
+
+        } else { // false
+
+            //Login Fail
+            Session::flash('error', 'Email atau password salah');
+            return redirect()->route('login');
+        }
+
+    }
+
+    public function showFormRegister()
+    {
+        return view('register');
+    }
+
+    public function register(Request $request)
+    {
+        $rules = [
+            'name'                  => 'required|min:3|max:35',
+            'email'                 => 'required|email|unique:users,email',
+            'password'              => 'required|confirmed'
+        ];
+
+        $messages = [
+            'name.required'         => 'Nama Lengkap wajib diisi',
+            'name.min'              => 'Nama lengkap minimal 3 karakter',
+            'name.max'              => 'Nama lengkap maksimal 35 karakter',
+            'email.required'        => 'Email wajib diisi',
+            'email.email'           => 'Email tidak valid',
+            'email.unique'          => 'Email sudah terdaftar',
+            'password.required'     => 'Password wajib diisi',
+            'password.confirmed'    => 'Password tidak sama dengan konfirmasi password'
+        ];
+
+        // $validator = Validator::make($request->all(), $rules, $messages);
+
+        // if($validator->fails()){
+        //     return redirect()->back()->withErrors($validator)->withInput($request->all);
+        // }
+
+        $user = new User;
+        $user->email = strtolower($request->email);
+        $user->first_name = $request->first_name;
+        $user->last_name = $request->last_name;
+        $user->username = strtolower($request->first_name.' '.$request->last_name);
+        $user->password = Hash::make($request->password);
+        $user->country = $request->country;
+        $user->province = $request->province;
+        $user->city = $request->city;
+        $user->postal_code = $request->postal_code;
+        $user->email_verified_at = \Carbon\Carbon::now();
+        $simpan = $user->save();
+
+        if($simpan){
+            Session::flash('success', 'Register berhasil! Silahkan login untuk mengakses data');
+            return redirect()->route('login');
+        } else {
+            Session::flash('errors', ['' => 'Register gagal! Silahkan ulangi beberapa saat lagi']);
+            return redirect()->route('register');
+        }
+        dd($request->all());
+    }
+
+    public function logout()
+    {
+        Auth::logout(); // menghapus session yang aktif
+        return redirect()->route('login');
+    }
+
+
+}
