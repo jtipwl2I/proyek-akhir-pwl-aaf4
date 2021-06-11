e6c08ce9dc594d53fbcb4e185afcca4e61d33492
Fajrin200 <muhammadfajrin412@gmail.com>
2021-06-12 06:24:14 +0700

    Menambahkan view admin nasabah

---
+++ app/Http/Controllers/Admin/NasabahController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class NasabahController extends Controller
+{
+    public function index()
+    {
+    	$users = User::all();
+    	return view('admin.nasabah', compact(['users']));
+    }
+}
