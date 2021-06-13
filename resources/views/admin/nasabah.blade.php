8b29131eb460f74717e17389a87e697f5e56afb8
fajrin200 <muhammadfajrin412@gmail.com>
2021-06-13 11:27:25 +0700

    buat form pencarian

--- resources/views/admin/nasabah.blade.php
+++ resources/views/admin/nasabah.blade.php
@@ -9,6 +9,13 @@
 					<th>
 						<input type="checkbox" class="" id="checkall">
 					</th>
+					<form class="form" method="get" action="{{ route('admin.nasabah.search') }}">
+						<div class="form-group w-100 mb-3">
+							<label for="search" class="d-block mr-2">Pencarian</label>
+							<input type="text" name="search" class="form-control w-75 d-inline" id="search" placeholder="Masukkan keyword">
+							<button type="submit" class="btn btn-primary mb-1">Cari</button>
+						</div>
+					</form>
 					<th>Foto</th>
 					<th>Nama depan</th>
 					<th>Nama belakang</th>
@@ -36,8 +43,12 @@
 		</table>
 	</div>
 	<div class="card-footer">
+		<button id="edit" class="btn btn-warning">Edit</button>
+		<button id="show" class="btn btn-info">Show</button>
 		<button id="delete" class="btn btn-danger">Hapus</button>
+		
 	</div>
+
 </div>
 @endsection
 @section('js')
