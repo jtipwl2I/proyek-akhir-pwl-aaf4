f61e1528f811a9e79452d49df74d54314a147fb7
fajrin200 <muhammadfajrin412@gmail.com>
2021-06-14 16:02:51 +0700

    Menambahkan form update data admin

--- resources/views/admin/setting.blade.php
+++ resources/views/admin/setting.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
-@section('section-header', 'Kelola Nasabah')
+@section('section-header', 'Setting')
 @section('content')
 <div class="card card-outline card-primary mt-3" id="userData">
-	<div class="card-header text-primary">Data Nasabah</div>
+	<div class="card-header text-primary">Data Admin</div>
 	<div class="card-body">
 		<div class="row">			<div class="form-group col-6">
 			<label for="first_name">First Name</label>
@@ -43,10 +43,81 @@
 </div>
 <div class="card-footer">
 	<div class="text-right">
-		<button class="btn btn-secondary" onclick="updateData();" id="btnEdit">Edit</button>
+		<button class="btn btn-secondary" onclick="$('#userData').hide(); $('#userForm').show()" id="btnEdit">Edit</button>
 	</div>
 </div>
 </div>
+
+<form class="card card-outline card-primary mt-3" id="userForm" enctype="multipart/form-data" method="post" style="display: none;">
+	<div class="card-header text-primary">Ubah Data Admin</div>
+	<div class="card-body">
+		<div class="row">			<div class="form-group col-6">
+			<label for="first_name">First Name</label>
+			<input id="first_name" type="text" class="form-control" name="first_name" value="{{ Auth::user()->first_name }}">
+		</div>
+		<div class="form-group col-6">
+			<label for="last_name">Last Name</label>
+			<input id="last_name" type="text" class="form-control" name="last_name" value="{{ Auth::user()->last_name }}">
+		</div>
+	</div>
+
+	<div class="form-group">
+		<label for="email">Email</label>
+		<input id="email" type="email" class="form-control" name="email" value="{{ Auth::user()->email }}">
+		<div class="invalid-feedback">
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-6">
+			<div class="form-group">
+				<label for="password">Password</label>
+				<input id="password" type="password" class="form-control" name="password">
+				<div class="invalid-feedback">
+				</div>
+			</div>
+		</div>
+		<div class="col-6">
+			<div class="form-group">
+				<label for="password">Password Confirmation</label>
+				<input id="password" type="password" class="form-control" name="password_confirmation">
+				<div class="invalid-feedback">
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="file">Gambar</label>
+		<input id="file" type="file" class="form-control" name="file">
+		<div class="invalid-feedback">
+		</div>
+	</div>
+	<div class="row">
+		<div class="form-group col-6">
+			<label>Country</label>
+			<input type="text" class="form-control" name="country" value="{{ Auth::user()->country }}">
+		</div>
+		<div class="form-group col-6">
+			<label>Province</label>
+			<input type="text" class="form-control" name="province" value="{{ Auth::user()->province }}">
+		</div>
+	</div>
+	<div class="row">
+		<div class="form-group col-6">
+			<label>City</label>
+			<input type="text" class="form-control" name="city" value="{{ Auth::user()->city }}">
+		</div>
+		<div class="form-group col-6">
+			<label>Postal Code</label>
+			<input type="text" class="form-control" name="postal_code" value="{{ Auth::user()->postal_code }}">
+		</div>
+	</div>
+</div>
+<div class="card-footer">
+	<div class="text-right">
+		<button class="btn btn-secondary" onclick="$('#userForm').hide(); $('#userData').show()" id="btnEdit">Batal</button>
+	</div>
+</div>
+</form>
 @endsection
 @section('js')
 <script type="text/javascript">
