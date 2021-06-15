a8a1961e9c1de13b6e51ccc64a35fad93e522c12
fajrin200 <muhammadfajrin412@gmail.com>
2021-06-15 14:24:04 +0700

    Menamabahkan card saldo pada dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.app')
 @section('section-header', 'Dashboard')
 @section('content')
+<div class="row">
+	<div class="col-12 col-sm-6 col-md-4 col-lg-4">
+		<div class="card card-outline card-warning">
+			<div class="card-body">
+				<label class="text-warning">Saldo anda</label>
+				<div class="text-dark">
+					Rp <h4 class="d-inline">1000000</h4>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 @endsection
