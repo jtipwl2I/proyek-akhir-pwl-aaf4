0f50529c282769194c40a249d04453a622853cbe
fajrin200 <muhammadfajrin412@gmail.com>
2021-06-15 14:20:40 +0700

    Merubah link sidebar dashboard

--- resources/views/layouts/sidebar-user.blade.php
+++ resources/views/layouts/sidebar-user.blade.php
@@ -7,8 +7,8 @@
       <a href="index.html">{{ substr(config('app.name'), 0, 2) }}</a>
     </div>
     <ul class="sidebar-menu">
-      <li class="nav-item dropdown">
-        <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+      <li class="nav-item">
+        <a href="{{ route('home') }}" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
       </li>
     </ul>
   </aside>
