5b961555a983d0ba6b4cb1c7a1c8e8d8e0086458
fajrin200 <muhammadfajrin412@gmail.com>
2021-06-19 10:23:42 +0700

    create riwayat transfer dan top up

---
+++ database/migrations/2021_06_19_031758_create_riwayat_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRiwayatTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('riwayat', function (Blueprint $table) {
+            $table->integer('jumlah_transfer');
+            $table->integer('jumlah_topUp');
+            $table->timestamps();
+            $table->integer('target_id');
+            $table->integer('from_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('riwayat');
+    }
+}
