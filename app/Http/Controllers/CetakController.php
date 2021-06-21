330292f87eb94d06c1d3e68931a54c93539b6ea0
fajrin200 <muhammadfajrin412@gmail.com>
2021-06-21 12:11:18 +0700

    file save to pdf

--- app/Http/Controllers/CetakController.php
+++ app/Http/Controllers/CetakController.php
@@ -11,16 +11,18 @@ class CetakController extends Controller
 {
 	public function index(Request $request)
 	{
+
 		$data = Riwayat::findOrFail($request->id);
 		$from = User::where('id', $data->from_id)->first();
 		$target = User::where('id', $data->target_id)->first();
 		$data->target = $target;
 		$data->from = $from;
-
+		//dd($data);
+	
 		// return $data;
 
 		// return view('Nasabah.cetak-pdf', compact(['data']));
 		$pdf = PDF::loadview('Nasabah.cetak-pdf', compact(['data']));
-		return $pdf->download('struk-saldo');
+		return $pdf->download('struk-saldo.pdf');
 	}
 }
