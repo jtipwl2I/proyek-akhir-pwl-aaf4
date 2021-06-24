3bc67239a10958ab0ae46e10c0fa299fb1374770
fajrin200 <muhammadfajrin412@gmail.com>
2021-06-25 04:27:43 +0700

    Memperbaiki data nasabah (croscek)

--- app/Http/Controllers/Admin/NasabahController.php
+++ app/Http/Controllers/Admin/NasabahController.php
@@ -11,7 +11,8 @@ class NasabahController extends Controller
 {
     public function index()
     {
-    	$users = User::all();
+        $users = new User();
+        $users = $users->getNasabah();
         return view('admin.admin', compact(['users']));
     }
 
@@ -22,29 +23,31 @@ class NasabahController extends Controller
 
     public function store(Request $request)
     {
-    	// $request->validate([
-    	// 	'first_name' => 'required|max:191',
-    	// 	'last_name' => 'required|max:191',
-    	// 	'email' => 'email|required|max:191',
-    	// 	'password' => 'required|confirmed',
-    	// 	'country' => 'required|max:191',
-    	// 	'province' => 'required|max:191',
-    	// 	'city' => 'required|max:191',
-    	// 	'postal_code' => 'required'
-    	// ]);
+        
+    	$request->validate([
+    		// 'first_name' => 'required|max:191',
+    		// 'last_name' => 'required|max:191',
+    		// 'email' => 'email|required|max:191',
+    		'password' => 'required|confirmed'
+    		// 'country' => 'required|max:191',
+    		// 'province' => 'required|max:191',
+    		// 'city' => 'required|max:191',
+    		// 'postal_code' => 'required'
+    	]);
 
-		$file = $request->file('file');
+		$file = $request->file('gambar');
 
 		$namafile = uniqid().'.'.$file->extension ();
 
 		$file->move('img/nasabah', $namafile);
-		$data = $request->all();
+		$data = $request->except('password_confirmation');
 		$data['gambar'] = $namafile;
     	$data['city'] = $request->city;
-    	$data['password'] = bcrypt($data['password']);
+    	$data['password'] = bcrypt($request->password);
     	$data['username'] = $request->first_name.' '.$request->last_name;
         $data['no_rek'] = random_int(0, 9).random_int(0, 9).random_int(0, 9).random_int(0, 9).random_int(0, 9);
 		//return $data;
+       // dd($data);
     	$create = User::create($data);
 
     	if ($create) {
@@ -58,7 +61,7 @@ class NasabahController extends Controller
     {
         $keyword = $request->search;
         $users = User::where('username', 'like', "%" . $keyword . "%")->get();
-        return view('admin.nasabah', compact('users'));
+        return view('admin.admin', compact('users'));
     }
     public function delete(Request $request)
     {
@@ -88,9 +91,10 @@ class NasabahController extends Controller
 
     public function update($id, Request $request)
     {
-        $data = $request->all();
+        
+        $data = $request->except('password_confirmation');
         $user = User::findOrFail($id);
-
+        dd($data);
         if ($request->password) {
             $data['password'] = bcrypt($request->password);
         } else {
@@ -100,10 +104,10 @@ class NasabahController extends Controller
         if ($request->file) {
 
             // Delete file lama
-            File::delete('img/nasabah/'. $user->gambar);
+          //  File::delete('img/nasabah/'. $user->gambar);
 
             // Upload file baru
-            $file = $request->file('file');
+            $file = $request->file('gambar');
             $namafile = uniqid().'.'.$file->extension();
             $file->move('img/nasabah', $namafile);
             $data['gambar'] = $namafile;
