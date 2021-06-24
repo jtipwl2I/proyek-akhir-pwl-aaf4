06e20bf1247a7d3e4964fb953c09072fdc2feaff
fajrin200 <muhammadfajrin412@gmail.com>
2021-06-25 04:25:29 +0700

    cetak file gmbar pada cetak pdf

--- app/Http/Controllers/CetakController.php
+++ app/Http/Controllers/CetakController.php
@@ -17,22 +17,47 @@ class CetakController extends Controller
 		$target = User::where('id', $data->target_id)->first();
 		$data->target = $target;
 		$data->from = $from;
+		
+
+		
+
 
 		// ubah format gambar jadi base64
 		$path = 'img/nasabah/' . $data->target->gambar;
 		$type = pathinfo($path, PATHINFO_EXTENSION);
 		$gambar = file_get_contents($path);
 		$data->target->gambar = 'data:image/'.$type.';base64,'.base64_encode($gambar);
-		$path = 'img/nasabah/' . $data->from->gambar;
-		$type = pathinfo($path, PATHINFO_EXTENSION);
-		$gambar = file_get_contents($path);
-		$data->from->gambar = 'data:image/'.$type.';base64,'.base64_encode($gambar);
-		//dd($data);
-	
-		// return $data;
 
-		// return view('Nasabah.cetak-pdf', compact(['data']));
-		$pdf = PDF::loadview('Nasabah.cetak-pdf', compact(['data']));
-		return $pdf->download('struk-saldo.pdf');
+		if($data->from){
+	$path = 'img/nasabah/' . $data->from->gambar;
+			$type = pathinfo($path, PATHINFO_EXTENSION);
+			$gambar = file_get_contents($path);
+			$data->from->gambar = 'data:image/'.$type.';base64,'.base64_encode($gambar);
+		}
+
+		
+	
+		
+	
+		// // return $data;
+
+		// // return view('Nasabah.cetak-pdf', compact(['data']));
+		// $pdf = PDF::loadview('Nasabah.cetak-pdf', compact(['data']));
+		// return $pdf->download('struk-saldo.pdf');
+
+		$options = new \Dompdf\Options();
+		$options->setIsRemoteEnabled(true);
+
+
+		$dompdf = new \Dompdf\Dompdf($options);
+
+
+
+		$data['data'] = $data;
+		$dompdf->loadHtml(view('Nasabah.cetak-pdf', $data));
+
+		$dompdf->setPaper('a4', 'portrait');
+		$dompdf->render();
+		$dompdf->stream('tes' . ".pdf", array('Attachment' => TRUE));
 	}
 }
