a27a5b6219d61996956e68adc957ae79ecee4c8e
fajrin200 <muhammadfajrin412@gmail.com>
2021-06-25 12:10:20 +0700

    pemberian warna pada halaman register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+ 
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
   <title>Register</title>
@@ -15,6 +16,11 @@
   <!-- Template CSS -->
   <link rel="stylesheet" href="../assets/css/style.css">
   <link rel="stylesheet" href="../assets/css/components.css">
+  <style>
+    body{
+    background-color:rgb(9, 224, 142) ;
+    }
+    </style>
 </head>
 
 <body>
