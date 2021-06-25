0886258c0dc439e032519410674786868508706f
fajrin200 <muhammadfajrin412@gmail.com>
2021-06-25 12:12:03 +0700

    pemberian warna pada halaman app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <style>
+    body{
+    background-color:blue;
+    }
+    </style>
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
   <title>Sistem Perbankan</title>
