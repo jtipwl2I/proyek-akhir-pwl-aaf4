d5cf13087e63cf0e9826010dcb2757b7ec76213d
fajrin200 <muhammadfajrin412@gmail.com>
2021-06-25 12:12:23 +0700

    pemberian warna pada halaman identitas

--- resources/views/Nasabah/identitasNasabah.blade.php
+++ resources/views/Nasabah/identitasNasabah.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 @section('section-header', 'Identitas Nasabah')
 @section('content')
+<style>
+    body{
+    background-color:rgb(19, 192, 183) ;
+    }
+    </style>
 <div class="card card-outline card-primary mt-3" id="userData">
 	<div class="card-header text-primary">Data Nasabah</div>
 	<div class="card-body">
